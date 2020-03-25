0a2b8d809ba00e47ad0386fad6ef4f25353ce89d
breakfasting <breakfast.ing@gmail.com>
2020-03-25 12:18:11 -0700

    Basic init, user auth model finished

---
+++ W7D2/music_app/app/models/user.rb
@@ -0,0 +1,38 @@
+class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  attr_reader :password
+
+  before_validation :ensure_session_token
+  #SPIRE
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil unless user
+
+    user.is_password?(password) ? user : nil
+  end
+
+  def password=(val)
+    @password = val
+    self.password_digest = BCrypt::Password.create(val)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+end
